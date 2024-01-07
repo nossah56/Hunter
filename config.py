2142719d3b9212ee8ec3fe259cab116b557f1f9e
Coi SR <150426205+nossah56@users.noreply.github.com>
2024-01-07 19:25:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://i.ibb.co/8xkN41z/aa4f62893f2a.jpg")
-START_IMG = getenv("START_IMG", "https://i.ibb.co/8xkN41z/aa4f62893f2a.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/a75fdf21d307b98b6f632.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/34ff76a850f28c2d39ca9.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/huntersource")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6596396930").split()))
 
 
-FAILED = "https://i.ibb.co/8xkN41z/aa4f62893f2a.jpg"
+FAILED = "https://telegra.ph/file/5f52312e9021cd9268077.jpg"
